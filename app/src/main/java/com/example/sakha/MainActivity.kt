79ee1b212732ded149829ff92b59bcc542e78cbc
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-08-31 23:25:22 +0530

    user detail form updates (Dropdown list creation)

--- app/src/main/java/com/example/sakha/MainActivity.kt
+++ app/src/main/java/com/example/sakha/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sakha
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
@@ -24,22 +25,54 @@ class MainActivity : AppCompatActivity() {
 //        finish()
 
         val states = listOf(
-            "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
-            "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
-            "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur",
-            "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab",
-            "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura",
-            "Uttar Pradesh", "Uttarakhand", "West Bengal"
+            "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
+            "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan",
+            "Sikkim", "Tamil Nadu", "Tripura", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal",
+            //Union Territories
+            "Andaman and Nicobar Islands", "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu",
+            "Delhi", "Jammu and Kashmir", "Ladakh", "Lakshadweep", "Puducherry"
         )
-        val txtField = findViewById<TextInputLayout>(R.id.state_dropdown_layout)
-        val dropdown = findViewById<AutoCompleteTextView>(R.id.stateDropdown)
 
-        txtField.isHintEnabled = true
-        dropdown.hint = ""
+        val districtsMap = mapOf(
+            "Andhra Pradesh" to listOf("Alluri Sitharama Raju", "Anakapalli", "Bapatla", "Dr. B. R. Ambedkar Konaseema", "East Godavari", "Eluru", "Guntur",
+                "Kakinada", "Krishna", "NTR", "Palnadu", "Parvathipuram Manyam", "Prakasam", "Srikakulam", "Sri Potti Sriramulu Nellore", "Visakhapatnam",
+                "Vizianagaram", "West Godavari", "Annamayya", "Anantapur", "Chittoor", "Kadapa (YSR)", "Kurnool", "Nandyal", "Sri Sathya Sai", "Tirupati")
+        )
 
+        val txtFieldState = findViewById<TextInputLayout>(R.id.state_dropdown_layout)
+        val dropdownState = findViewById<AutoCompleteTextView>(R.id.stateDropdown)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, states.sorted())
-        dropdown.setAdapter(adapter)
-        dropdown.threshold = 1
+        val txtFieldDistrict = findViewById<TextInputLayout>(R.id.district_dropdown_layout)
+        val dropdownDistrict = findViewById<AutoCompleteTextView>(R.id.districtDropdown)
+
+        val txtFieldVillage = findViewById<TextInputLayout>(R.id.village_dropdown_layout)
+        val dropdownVillage = findViewById<AutoCompleteTextView>(R.id.villageDropdown)
+
+        txtFieldState.isHintEnabled = true
+        dropdownState.hint = ""
+
+        txtFieldDistrict.isHintEnabled = true
+        dropdownDistrict.hint = ""
+
+        txtFieldVillage.isHintEnabled = true
+        dropdownVillage.hint = ""
+
+        val stateAdapter = ArrayAdapter(this, R.layout.custom_dropdown_item, states.sorted())
+        dropdownState.setAdapter(stateAdapter)
+        dropdownState.setDropDownBackgroundResource(R.color.dropdown_bg)
+        dropdownState.dropDownHeight = WindowManager.LayoutParams.WRAP_CONTENT
+        dropdownState.dropDownWidth = WindowManager.LayoutParams.MATCH_PARENT
+
+        dropdownState.threshold = 1
+
+        dropdownState.setOnItemClickListener { _, _, position, _ ->
+            val selectedState = stateAdapter.getItem(position)
+
+            val districts = districtsMap[selectedState] ?: emptyList()
+
+            val districtAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, districts)
+            dropdownDistrict.setAdapter(districtAdapter)
+            dropdownDistrict.setText("", false) // clear old selection
+        }
     }
 }
