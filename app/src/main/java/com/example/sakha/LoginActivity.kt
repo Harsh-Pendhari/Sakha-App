0de84bd7e65b8973dd1f81a76573fdef1845bd56
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-08-31 14:41:39 +0530

    error fix

--- app/src/main/java/com/example/sakha/LoginActivity.kt
+++ app/src/main/java/com/example/sakha/LoginActivity.kt
@@ -15,11 +15,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
         val forgotPassword = findViewById<Button>(R.id.forgotPassword)
         val errorMsg = findViewById<TextView>(R.id.error_msg)
--- app/src/main/java/com/example/sakha/RegisterActivity.kt
+++ app/src/main/java/com/example/sakha/RegisterActivity.kt
@@ -15,7 +15,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
