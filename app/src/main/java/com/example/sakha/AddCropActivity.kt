7ed8d3e93f3a420b98b9345c629de6554c7b121f
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-14 17:56:27 +0530

    update

--- app/src/main/java/com/example/sakha/AddCropActivity.kt
+++ app/src/main/java/com/example/sakha/AddCropActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sakha
 
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,10 @@ class AddCropActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_addcrop)
+
+        val textView = findViewById<TextView>(R.id.nocropText)
+        textView.text = Html.fromHtml(getString(R.string.nocroptxt), Html.FROM_HTML_MODE_LEGACY)
+
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
