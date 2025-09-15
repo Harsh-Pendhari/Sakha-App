fc31be767d605a527eec5bd36ed056861b3e18d0
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-15 20:37:51 +0530

    update

--- app/src/main/java/com/example/sakha/IrrigationMethodsActivity.kt
+++ app/src/main/java/com/example/sakha/IrrigationMethodsActivity.kt
@@ -1,35 +1,21 @@
 package com.example.sakha
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputLayout
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class IrrigationMethodsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_irrigationmethods)
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        startActivity(Intent(this, UserdetailsformActivity::class.java))
-        finish()
     }
 }
