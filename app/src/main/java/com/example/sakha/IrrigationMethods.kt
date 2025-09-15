148e20d5f79e0eb2559bc78c3df808e0ad97a447
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-15 21:43:44 +0530

    updates

---
+++ app/src/main/java/com/example/sakha/IrrigationMethods.kt
@@ -0,0 +1,7 @@
+package com.example.sakha
+
+data class IrrigationMethods(
+    val methodName: String,
+    val description: String,
+    val icon: Int
+)
--- app/src/main/java/com/example/sakha/IrrigationMethodsActivity.kt
+++ app/src/main/java/com/example/sakha/IrrigationMethodsActivity.kt
@@ -7,12 +7,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 
-data class IrrigationMethods(
-    val methodName: String,
-    val description: String,
-    val icon: Int
-)
-
 class IrrigationMethodsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +21,14 @@ class IrrigationMethodsActivity : AppCompatActivity() {
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerViewIrrigation)
 
-        // Example crop list
         val irrigationMethods = listOf(
             IrrigationMethods("Drip Irrigation", "Delivers water directly into the roots, saves water, suitable for vegetables and orchards.", R.drawable.drip_irrigation_icon ),
             IrrigationMethods("Sprinkler Irrigation", "Sprays water like natural rainfall, good for cereals & group crops.", R.drawable.sprinkler_irrigation_icon),
             IrrigationMethods("Surface Irrigation","Water flows over the field surface by gravity, suitable for paddy & wheat.", R.drawable.surface_irrigation_icon),
             IrrigationMethods("Manual Irrigation","Traditional method using buckets, pipes, or cans.", R.drawable.manual_irrigation_icon),
             IrrigationMethods("Center Pivot Irrigation","Rotates around a central pivot, spraying water in a circular pattern.", R.drawable.centre_pivot_irrigation_icon),
-            IrrigationMethods("Lateral Move Irrigation","Moves laterally across the field, spraying water uniformly.", R.drawable.manual_irrigation_icon),
-            IrrigationMethods("Sub-Irrigation","Delivers water from below the soil surface, keeps roots moist, reduces evaporation.", R.drawable.manual_irrigation_icon),
+            IrrigationMethods("Lateral Move Irrigation","Moves laterally across the field, spraying water uniformly.", R.drawable.lateral_move_irrigation_icon),
+            IrrigationMethods("Sub-Irrigation","Delivers water from below the soil surface, keeps roots moist, reduces evaporation.", R.drawable.sub_irrigation_icon),
         )
 
         recyclerView.adapter = IrrigationMethodsAdapter(irrigationMethods)
