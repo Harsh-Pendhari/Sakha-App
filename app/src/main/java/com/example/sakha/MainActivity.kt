696ca3247b732571c2174f827316d310ced5b36b
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-15 20:39:08 +0530

    update

--- app/src/main/java/com/example/sakha/MainActivity.kt
+++ app/src/main/java/com/example/sakha/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        startActivity(Intent(this, UserdetailsformActivity::class.java))
+        startActivity(Intent(this, IrrigationMethodsActivity::class.java))
         finish()
     }
 }
