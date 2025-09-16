ff42e7a84208889963c13746a7fa62523d76a2c9
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-16 18:54:01 +0530

    update

--- app/src/main/java/com/example/sakha/PesticidesActivity.kt
+++ app/src/main/java/com/example/sakha/PesticidesActivity.kt
@@ -1,4 +1,20 @@
 package com.example.sakha
 
-class PesticidesActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PesticidesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pesticides)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
