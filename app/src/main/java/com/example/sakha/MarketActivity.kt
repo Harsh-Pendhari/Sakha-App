2d87d244a9269d93f6039de327f6367a1aff71d6
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-16 21:16:14 +0530

    update

--- app/src/main/java/com/example/sakha/MarketActivity.kt
+++ app/src/main/java/com/example/sakha/MarketActivity.kt
@@ -2,21 +2,12 @@ package com.example.sakha
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputLayout
-import org.json.JSONObject
+
 
 class MarketActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +18,49 @@ class MarketActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val fruitBTN = findViewById<Button>(R.id.fruits_and_vegetables)
+        val fruitBTNLink = "https://go4fresh.com/"
+        val fruitTitle = getString(R.string.fruitandvegetables)
+
+        fruitBTN.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", fruitBTNLink)
+            intent.putExtra("TITLE", fruitTitle)
+            startActivity(intent)
+        }
+
+        val cropBTN = findViewById<Button>(R.id.crops)
+        val cropBTNLink = "https://go4fresh.com/"
+        val croptTitle = getString(R.string.crops)
+
+        cropBTN.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", cropBTNLink)
+            intent.putExtra("TITLE", croptTitle)
+            startActivity(intent)
+        }
+
+        val liveStockBTN = findViewById<Button>(R.id.livestock)
+        val liveStockBTNLink = "https://go4fresh.com/"
+        val liveStockTitle = getString(R.string.livestock)
+
+        liveStockBTN.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", liveStockBTNLink)
+            intent.putExtra("TITLE", liveStockTitle)
+            startActivity(intent)
+        }
+
+        val machineryBTN = findViewById<Button>(R.id.machinery)
+        val machineryBTNLink = "https://go4fresh.com/"
+        val machineryTitle = getString(R.string.machinery)
+
+        machineryBTN.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            intent.putExtra("URL", machineryBTNLink)
+            intent.putExtra("TITLE", machineryTitle)
+            startActivity(intent)
         }
     }
 }
