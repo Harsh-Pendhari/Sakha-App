22b06febf8fbcf10aee96fa21c8326ac29dd5ae1
Harsh Pendhari <harshpendhari2901@gmail.com>
2025-09-16 19:23:56 +0530

    updaes

--- app/src/main/java/com/example/sakha/PesticidesActivity.kt
+++ app/src/main/java/com/example/sakha/PesticidesActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sakha
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class PesticidesActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val insecticideBTN = findViewById<LinearLayout>(R.id.insecticides_btn)
+        val insecticideBTNLink = "https://mankindag.com/product/insecticide/"
+
+        insecticideBTN.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
